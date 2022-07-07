619db0dd7b499c0df276c8555b46659aab6be5ed
Bazil Korath <korathbasil@live.com>
2022-07-08 03:13:52 +0530

    feat : add array flatten util fn

--- lib/src/heimdall_base.dart
+++ lib/src/heimdall_base.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:heimdall/src/http/http.dart';
 import 'package:heimdall/src/context.dart';
+import 'package:heimdall/src/middlewares/middleware.dart';
 import 'package:heimdall/src/router/router.dart';
 import 'package:heimdall/src/util/handler.dart';
 import 'package:heimdall/src/util/router.dart';
@@ -17,6 +18,10 @@ class Heimdall {
 
   enableCors() {
     shouldEnableCors = true;
+  }
+
+  use(RequestHandler h) {
+    useMiddleware(h);
   }
 
   _configCors(Context ctx) {
@@ -61,4 +66,25 @@ class Heimdall {
   }
 }
 
-typedef RequestHandler = void Function(Context ctx);
+typedef RequestHandler = void Function(Context ctx, {NextFunction next});
+
+typedef NextFunction = void Function(Context ctx);
+
+void useMiddleware(dynamic fn) {
+  var offset = 0;
+  var path = "/";
+
+  if (fn is! Function) {
+    var arg = fn;
+
+    // drilling to last value of arg
+    while (arg is List<dynamic> && arg.isNotEmpty) {
+      arg = arg[0];
+    }
+
+    if (fn is! Function) {
+      offset = 1;
+      path = fn;
+    }
+  }
+}
---
+++ lib/src/util/list_flatten.dart
@@ -0,0 +1,17 @@
+List<dynamic> flattenForever(List<dynamic> list, List<dynamic> result) {
+  for (var i = 0; i < list.length; i++) {
+    var item = list[i];
+
+    if (item is List<dynamic>) {
+      flattenForever(item, result);
+    } else {
+      result.add(item);
+    }
+  }
+
+  return result;
+}
+
+List<dynamic> listFlatten(List<dynamic> list) {
+  return flattenForever(list, []);
+}
